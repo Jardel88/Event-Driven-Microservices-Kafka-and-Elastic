953cd74f8fcf71ae3a77a8f9a2a5c9a8ddd50929
jldv <jardel.gomes@gft.com>
2022-03-29 10:24:19 -0300

    Upload projeto inicial

---
+++ microservices-kafka/src/test/java/com/microserviceskafka/MicroservicesKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microserviceskafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
