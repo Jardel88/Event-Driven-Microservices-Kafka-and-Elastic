3133e721bed2bbd88d6250b32b66c424ffcbe1dc
jldv <jardel.gomes@gft.com>
2022-04-25 15:15:48 -0300

    Streaming com twitter4j

--- microservices-kafka/twitter-to-kafka-service/src/main/java/com/microservices/twitter/to/kafka/services/TwitterToKafkaServiceApplication.java
+++ microservices-kafka/twitter-to-kafka-service/src/main/java/com/microservices/twitter/to/kafka/services/TwitterToKafkaServiceApplication.java
@@ -1,6 +1,7 @@
 package com.microservices.twitter.to.kafka.services;
 
 import com.microservices.twitter.to.kafka.services.config.TwitterToKafkaServiceConfigData;
+import com.microservices.twitter.to.kafka.services.runner.StreamRunner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
@@ -16,8 +17,11 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
     private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
 
-    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData) {
+    private final StreamRunner streamRunner;
+
+    public TwitterToKafkaServiceApplication(TwitterToKafkaServiceConfigData configData, StreamRunner runner) {
         this.twitterToKafkaServiceConfigData = configData;
+        this.streamRunner = runner;
     }
 
     public static void main(String[] args) {
@@ -29,5 +33,6 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
         LOG.info("App starts...");
         LOG.info(Arrays.toString((twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[]{}))));
         LOG.info(twitterToKafkaServiceConfigData.getWelcomeMessage());
+        streamRunner.start();
     }
 }
